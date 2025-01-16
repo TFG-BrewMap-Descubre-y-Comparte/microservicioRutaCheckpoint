e8ab82d7d19bdcf65089e88928711023dea546d2
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2025-01-16 17:27:55 +0100

    Añadiendo proyecto microservicioRutaCheckpoint

---
+++ src/main/java/com/project/routeCheckpoint/RouteCheckpointApplication.java
@@ -0,0 +1,13 @@
+package com.project.routeCheckpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteCheckpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteCheckpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/routeCheckpoint/RouteCheckpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.routeCheckpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteCheckpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
