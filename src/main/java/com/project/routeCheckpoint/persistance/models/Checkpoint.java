d1ef287883576038e1f3f5260515161f79f51e54
fvaquero <fvaquero@uc-3-325.AluVedruna.local>
2025-01-16 19:29:16 +0100

    modificacion y validaciones modelos

--- src/main/java/com/project/routeCheckpoint/persistance/models/Checkpoint.java
+++ src/main/java/com/project/routeCheckpoint/persistance/models/Checkpoint.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -21,12 +23,15 @@ public class Checkpoint {
 	private int checkpointId;
 	
 	@Column(name = "length")
+    @Digits(integer = 3, fraction = 6)
     private BigDecimal length;
 
     @Column(name = "latitude")
+    @Digits(integer = 3, fraction = 6)
     private BigDecimal latitude;
     
     @Column(name="name")
+    @Size(min = 2, max = 100, message = "La descripcion debe tener entre 2 y 100 caracteres.")
     private String nameCheckpoint;
 
 }
--- src/main/java/com/project/routeCheckpoint/persistance/models/Route.java
+++ src/main/java/com/project/routeCheckpoint/persistance/models/Route.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.Size;
 
 public class Route {
 	
@@ -14,6 +15,7 @@ public class Route {
 	private int routeId;
 	
 	@Column(name = "name", unique = true)
+    @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres.")
     private String name;
 	
 	@Column(name = "description")
